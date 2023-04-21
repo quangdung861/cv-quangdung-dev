d7d48ddf27aa7aacde791c75d155ba59ccf2cc72
quangdung861 <quangdung861@gmail.com>
2023-04-21 17:40:00 +0700

    Update appbar mobile.

--- src/App.jsx
+++ src/App.jsx
@@ -76,36 +76,42 @@ function App() {
           <a
             className="sidebar-item list-group-item list-group-item-action"
             href="#list-item-1"
+            onClick={() => setShowNavListMobile(false)}
           >
             ABOUT
           </a>
           <a
             className="sidebar-item list-group-item list-group-item-action"
             href="#list-item-2"
+            onClick={() => setShowNavListMobile(false)}
           >
             PROJECTS
           </a>
           <a
             className="sidebar-item list-group-item list-group-item-action"
             href="#list-item-3"
+            onClick={() => setShowNavListMobile(false)}
           >
             EDUCATION
           </a>
           <a
             className="sidebar-item list-group-item list-group-item-action"
             href="#list-item-4"
+            onClick={() => setShowNavListMobile(false)}
           >
             SKILLS
           </a>
           <a
             className="sidebar-item list-group-item list-group-item-action"
             href="#list-item-5"
+            onClick={() => setShowNavListMobile(false)}
           >
             INTERESTS
           </a>
           <a
             className="sidebar-item list-group-item list-group-item-action"
             href="#list-item-6"
+            onClick={() => setShowNavListMobile(false)}
           >
             THE END
           </a>
