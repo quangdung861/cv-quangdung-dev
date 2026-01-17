0030658dabcf773207e640a80e31b494365a196c
quangdung861 <quangdung861@gmail.com>
2026-01-17 07:35:56 +0700

    fix: displaying error of navbar #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
-
+import * as bootstrap from "bootstrap";
 import Sidebar from "./components/Sidebar";
 import SidebarMobile from "./components/SidebarMobile";
 
